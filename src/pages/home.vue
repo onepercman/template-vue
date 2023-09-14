91a9d43f0d2b0534d8b515031b9593426432e224
Onepercman <onepercman@gmail.com>
2023-09-14 18:05:32 +0700

    🚀 update button example

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,7 +3,9 @@ import Button from "@/libs/ui/button.vue"
 </script>
 
 <template>
-  <div class="border-primary/50 rounded border border-dashed p-6">
-    <Button variant="primary" left-icon="hi-academic-cap"> Click </Button>
+  <div class="p-6">
+    <div class="border-line rounded border p-6">
+      <Button>Button</Button>
+    </div>
   </div>
 </template>
